3ec918104b738fc89aeca9a8791e8a683bd8d71a
dipanshu2707 <singhdp2707@gmail.com>
2026-05-04 23:32:17 +0530

    Fix typos, update execution counts, and add new files for file I/O examples and a game project
    - Corrected "user defined function" to "User defined function" in chapter8.ipynb
    - Updated execution count for a code cell in chapter8.ipynb
    - Changed output value in a code cell in chapter8.ipynb
    - Added chapter9.ipynb with content on file I/O, including reading and writing files
    - Created filt.txt, hiscore.txt, more_file, myfile.txt, and poem.txt with example content for file operations
    - Introduced project1.ipynb for a simple game implementation with user input and random choices

--- chapter2.py
+++ chapter2.py
@@ -1,1 +1,36 @@
-3. 
+# f = open("more_file")
+# lines1 = f.readlines()
+# print(lines1, type(lines1))
+
+
+# lines2 = f.readlines()
+# print(lines2, type(lines2))
+
+# lines3 = f.readlines()
+# print(lines3, type(lines3))
+
+# f.close()
+
+
+
+import random
+
+def game():
+    print("you are playing the game")
+
+    score =  random.randint(1,40)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore != ""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0    
+    print(f"Your score is: {score}")
+    if(score > hiscore):
+
+        with open("hiscore.txt","w") as f:
+            f.write(str(score))
+
+    return score
+game()
